e47b18b59915ed0d669178492b008be15c9e1194
zkils <zkils8302@gmail.com>
2014-05-10 13:31:02 +0900

    ladder

--- ladder.js
+++ ladder.js
@@ -1,4 +1,14 @@
 var ladder = {};
+$(document).ready(function(e){
+    $("input").on("blur",function(e){
+        if(parseInt($(this).val()) <= 0){
+            $(this).val(1);
+        }else{
+            return false;
+        }
+    });
+});
+
 function pushLadder(){
     var $row = $("input[name='row']");
     var $col = $("input[name='column']");
@@ -12,7 +22,6 @@ function pushLadder(){
     }else{
         ladder[colVal] =[rowVal];
     }
-
     $row.val(null);
     $col.val(null);
 };
@@ -26,26 +35,28 @@ function checkDuplicate(val,arr){
     return true;
 };
 
+function selectLadder(){
+    var $startPoint = $("input[name='start-point']");
+    var startPoint = [$startPoint.val(),0];
+    if(checkAvailable($startPoint.val())){
+        searchNextLadder(startPoint);
+        $("#result").text("도착지는 " + startPoint[0] + " 입니다.");
+    }else{
+        $("#result").text("사다리가 없습니다.");
+    }
+};
 
-function validateLadder(){
-    var $input = $("input[type='number']");
-    if(parseInt($input.val()) <= 0){
-        $input.val(1);
+function checkAvailable(val){
+    if(ladder[val] || ladder[val-1]){
+        return true;
     }else{
         return false;
     }
 };
 
-function selectLadder(){
-    var $startPoint = $("input[name='start-point']");
-    var startPoint = [$startPoint.val(),0];
-    searchNextLadder(startPoint);
-    $("#result").text("도착지는 " + startPoint[0] + " !");
-};
-
 function searchNextLadder(startPoint){
     var range = ladder[startPoint[0]];
-    var leftMin = 999 , rightMin = 999;
+    var leftMin , rightMin;
     console.log(startPoint);
     for(var i in range){
         if(range[i]>startPoint[1]){
@@ -60,15 +71,17 @@ function searchNextLadder(startPoint){
             break;
         }
     }
-    if(leftMin > rightMin){ //go right ladder
+
+    if(typeof leftMin === 'undefined' && typeof rightMin === 'undefined'){
+        return startPoint;
+    }else if(leftMin > rightMin || typeof leftMin === 'undefined'){ //go right ladder
         startPoint[0]++;
         startPoint[1] = rightMin;
         searchNextLadder(startPoint);
-    }else if(leftMin < rightMin){
+    }else if(leftMin < rightMin || typeof rightMin === 'undefined'){
         startPoint[0]--;
         startPoint[1] = leftMin;
         searchNextLadder(startPoint);
-    }else{
-        return startPoint;
     }
+
 };
